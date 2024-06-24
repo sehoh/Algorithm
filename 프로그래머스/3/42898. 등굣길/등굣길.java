68228c270b94344350b2c9cd9961a096b0609a98
sehoh <140302648+sehoh@users.noreply.github.com>
2024-06-24 10:04:29 +0900

    [level 3] Title: 등굣길, Time: 0.92 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,19 +2,23 @@ import java.util.*;
 
 class Solution {
     private final int[][] memo = new int[101][101];
-    private int count(int x, int y, int w, int h, boolean[][] isPuddle) {
-        if(x > w || y > h) return 0;
-        if(isPuddle[y][x]) return 0;
 
-        if(memo[y][x] > 0) return memo[y][x];
+    private int count(int x, int y, int w, int h, boolean[][] isPuddle) {
+        // 범위밖이니?
+        if(x > w || y > h) return 0;
+        // 웅덩이니?
+        if(isPuddle[y][x]) return 0;
+        // 이미 구한 값이니?
+        if(memo[x][y]  != -1) return memo[x][y];
+        // 학교에 도달했니?
         if(x == w && y == h) return 1;
 
-        int total = count(x + 1, y, w, h, isPuddle)
-                + count(x, y + 1, w, h, isPuddle);
-        return memo[y][x] = total % 1000000007;
+        int total = count(x + 1, y, w, h, isPuddle) +
+                count(x, y + 1, w, h, isPuddle);
+
+        return memo[x][y] = total % 1000000007;
     }
-
-
+    
     public int solution(int m, int n, int[][] puddles) {
         for (int[] row : memo) {
             Arrays.fill(row, -1);
@@ -27,5 +31,4 @@ class Solution {
 
         return count(1, 1, m, n, isPuddle);
     }
-
 }
