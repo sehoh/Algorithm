aa42ad8ff97daa7922bc2dbe15a1c9bc0ad40f51
sehoh <140302648+sehoh@users.noreply.github.com>
2024-07-24 11:21:42 +0900

    [level 2] Title: 거리두기 확인하기, Time: 3.38 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,76 +1,95 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static List<char[][]> list;
     
+    private boolean isInRange(int nx, int ny) {
+        return (ny >= 0 && ny < 5 && nx >= 0 && nx < 5);
+    }
+
+    private boolean isWall(int nx, int ny, char[][] board) {
+        return board[ny][nx] == 'X';
+    }
+
     public int[] solution(String[][] places) {
-        int[] result = new int[5];
+        int n = places.length;
+        int[] answer = new int[n];
 
-        list = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            list.add(new char[5][5]);
+        for (int i = 0; i < n; i++) {
+            char[][] board = genBoard(places[i]);
+            answer[i] = check(board);
         }
+        return answer;
+    }
+
+    private int check(char[][] board) {
+        // 한 번이라도 P가 나온다면 0 return하고 종료
+        final int[] dy = {-1, 0, 1, 0};
+        final int[] dx = {0, 1, 0, -1};
+        boolean[][] visited = new boolean[5][5];
 
         for (int i = 0; i < 5; i++) {
-            String[] place = places[i];
             for (int j = 0; j < 5; j++) {
-                String row = place[j];
-                for (int k = 0; k < 5; k++) {
-                    list.get(i)[j][k] = row.charAt(k);
-                }
-            }
-        }
+                if (board[i][j] == 'P') {
+                    // BFS 실행
+                    Point start = new Point(j, i);
+                    visited[i][j] = true;
 
-        for (int i =0; i< 5; i++) {
-            if(!check(i)){
-                result[i] = 0;
-            } else {
-                result[i] = 1;
-            }
-        }
+                    Queue<Point> queue = new ArrayDeque<>();
+                    queue.add(start);
 
+                    int L = 0;
 
-        return result;
-    }
-    private boolean check(int i) {
-        char[][] place = list.get(i);
-        for (int x = 0; x < 5; x++) {
-            for (int y = 0; y < 5; y++) {
-                if (place[x][y] == 'P') {
-                    if (!isDistancing(i, x, y)) {
-                        return false;
+                    while (!queue.isEmpty() && L < 2) {
+                        int len = queue.size();
+
+                        for (int k = 0; k < len; k++) {
+                            Point curr = queue.poll();
+
+                            for (int d = 0; d < 4; d++) {
+                                int nx = curr.x + dx[d];
+                                int ny = curr.y + dy[d];
+
+                                // 범위, 벽검사
+                                if (isInRange(nx, ny) && !isWall(nx, ny, board)) {
+                                    // 방문했는지 검사
+                                    if (!visited[ny][nx]) {
+                                        // (nx, ny)가 P 라면 거리두기를 지키지 않는것임!
+                                        if(board[ny][nx] == 'P') return 0;
+                                        visited[ny][nx] = true;
+                                        queue.add(new Point(nx, ny));
+                                    }
+                                }
+                            }
+                        }
+                        L++;
                     }
                 }
             }
         }
-        return true;
+
+        return 1;
     }
-public boolean isDistancing(int i, int x, int y) {
-        char[][] place = list.get(i);
-        // 맨해튼 거리 1에 대해 탐색
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && place[nx][ny] != 'X') {
-                if(place[nx][ny] == 'P') return false;
 
-                // 맨해튼 거리 2에 대해 탐색
-                for (int d2 = 0; d2 < 4; d2++) {
-                    // 역방향이면 continue
-                    if(d2 == ((d+2) % 4)) continue;
-                    int n2x = nx + dx[d2];
-                    int n2y = ny + dy[d2];
-                    if (n2x >= 0 && n2x < 5 && n2y >= 0 && n2y < 5 && place[n2x][n2y] != 'X') {
-                        if(place[n2x][n2y] == 'P') return false;
-                    }
-                }
+    private char[][] genBoard(String[] place) {
+        char[][] board = new char[5][5];
 
+        for (int i = 0; i < 5; i++) {
+            String str = place[i];
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = str.charAt(j);
             }
         }
 
-        return true;
+        return board;
     }
-    
+
+    private class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
